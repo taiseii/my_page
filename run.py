2d7a88c685056aed30b034ecd2076fb0103191f2
taiseii <taiseii@protonmail.com>
2019-11-22 11:17:42 +0100

    mini change

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flaskApp import app
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9999)#debug is true when this is ran directly 
+    app.run(debug=True)#debug is true when this is ran directly 
     
